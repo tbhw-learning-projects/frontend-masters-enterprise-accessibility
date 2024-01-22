407d8d25bf62f8a0529ce1d0dfac3f3ef39170bd
Tanner B. Hess Webber <tb@hesswebber.com>
2024-01-22 14:14:50 -0500

    exercise: adds screen reader accessible slideshow

--- components/Slideshow.tsx
+++ components/Slideshow.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 
 const Slideshow = ({ imageData = [], imagePath = '/slideshow/' }) => {
 	let [currentSlideIndex, setCurrentSlideIndex] = useState(0);
@@ -6,27 +6,27 @@ const Slideshow = ({ imageData = [], imagePath = '/slideshow/' }) => {
 
 	const btnFullScreenRef = useRef(null);
 	const btnCloseRef = useRef(null);
-	const slideshowRef = useRef(null);
+	const slideshowWrapperRef = useRef(null);
 
 	console.log(imageData);
 
-	const decrementSlide = () => {
+	const decrementSlide = useCallback(() => {
 		if (currentSlideIndex > 0) {
 			setCurrentSlideIndex(currentSlideIndex - 1);
 		} else {
 			setCurrentSlideIndex(imageData.length - 1);
 		}
-	};
-	const incrementSlide = () => {
+	}, [currentSlideIndex, imageData.length]);
+	const incrementSlide = useCallback(() => {
 		if (currentSlideIndex < imageData.length - 1) {
 			setCurrentSlideIndex(currentSlideIndex + 1);
 		} else {
 			setCurrentSlideIndex(0);
 		}
-	};
-	const changeSlide = (index) => {
+	}, [currentSlideIndex, imageData.length]);
+	const changeSlide = useCallback((index: number) => {
 		setCurrentSlideIndex(index);
-	};
+	}, []);
 	const enterFullScreen = () => {
 		setFullScreenMode(true);
 	};
@@ -34,47 +34,84 @@ const Slideshow = ({ imageData = [], imagePath = '/slideshow/' }) => {
 		setFullScreenMode(false);
 	};
 	const handleScreenClick = (event) => {
-		if (!slideshowRef.current.contains(event.target)) {
-			setFullScreenMode(false);
+		if (slideshowWrapperRef.current === event.target) {
+			closeFullScreen();
 		}
 	};
+
+	useEffect(() => {
+		function handleKeys(event: KeyboardEvent) {
+			if (event.code === 'Escape') {
+				closeFullScreen();
+			}
+			if (['ArrowRight', 'ArrowUp'].includes(event.code)) {
+				incrementSlide();
+			}
+			if (['ArrowLeft', 'ArrowDown'].includes(event.code)) {
+				decrementSlide();
+			}
+			if (['Numpad', 'Digit'].some((substring) => event.code.includes(substring))) {
+				const keyValue = Number(event.code[event.code.length - 1]);
+				const indexValue = keyValue - 1;
+				const overflowProtectedIndex = (indexValue + imageData.length) % imageData.length;
+				changeSlide(overflowProtectedIndex);
+			}
+		}
+
+		window.addEventListener('keydown', handleKeys);
+
+		return () => window.removeEventListener('keydown', handleKeys);
+	}, [incrementSlide, decrementSlide, changeSlide, imageData.length]);
+
 	return (
 		<>
-			<div className="btn-slideshow-fullscreen" onClick={enterFullScreen} ref={btnFullScreenRef}>
+			<button
+				className="btn-slideshow-fullscreen"
+				onClick={fullScreenMode ? closeFullScreen : enterFullScreen}
+				ref={btnFullScreenRef}
+				aria-label={`${fullScreenMode ? 'Exit' : 'Enter'} full screen view`}>
 				<span className="icon"></span>
-			</div>
+			</button>
 			<div
+				ref={slideshowWrapperRef}
 				className={`inspiration-slideshow ${fullScreenMode ? 'fullscreen' : ''}`}
-				onClick={(event) => handleScreenClick(event)}>
-				<div className="slideshow-container" ref={slideshowRef}>
+				onClick={(event) => handleScreenClick(event)}
+				onKeyDown={(e) => console.log(e)}>
+				<div
+					className="slideshow-container"
+					aria-live="polite"
+					aria-roledescription="Image Slideshow"
+					role={fullScreenMode ? 'application' : 'region'}>
 					{imageData &&
 						imageData.map((image, index) => {
 							return (
-								<div className={`slide fade ${currentSlideIndex === index ? 'active' : ''}`} key={index}>
-									<div className="numbertext">
+								<figure className={`slide fade ${currentSlideIndex === index ? 'active' : ''}`} key={index}>
+									<p className="numbertext">
 										{index + 1} / {imageData.length}
-									</div>
+									</p>
 									<img src={`${imagePath}${image.src}`} alt={image.alt} style={{ width: '100%' }} />
-									<div className="text">{image.caption}</div>
-								</div>
+									<figcaption className="text">{image.caption}</figcaption>
+								</figure>
 							);
 						})}
 
-					<a className="prev" onClick={() => decrementSlide()}>
+					<button aria-label="Previous photo" className="prev" onClick={() => decrementSlide()}>
 						&#10094;
-					</a>
-					<a className="next" onClick={() => incrementSlide()}>
+					</button>
+					<button aria-label="Next photo" className="next" onClick={() => incrementSlide()}>
 						&#10095;
-					</a>
+					</button>
 				</div>
 				<br />
 
 				<ul className="dots">
 					{imageData.map((image, index) => (
-						<li
-							className={`dot ${currentSlideIndex === index ? 'active' : ''}`}
-							key={index}
-							onClick={() => changeSlide(index)}></li>
+						<li key={index}>
+							<button
+								aria-label={`Go to image ${index + 1}`}
+								className={`dot ${currentSlideIndex === index ? 'active' : ''}`}
+								onClick={() => changeSlide(index)}></button>
+						</li>
 					))}
 				</ul>
 			</div>
