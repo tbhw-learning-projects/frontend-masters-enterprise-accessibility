f078f9b72f586c34bcf370c530c6d3c8ab719e86
Tanner B. Hess Webber <tb@hesswebber.com>
2024-01-25 02:23:00 -0500

    exercise(5): adds portal switcher with tests

--- components/PortalSwitcher.jsx
+++ components/PortalSwitcher.jsx
@@ -1,35 +1,56 @@
-import { useCallback, useRef, useState } from 'react';
-import { IconDownArrow } from './Icons';
+import { useState, useRef } from 'react';
+import { FaChevronDown } from 'react-icons/fa';
 
-const PortalSwitcher = ({ children, ...props }) => {
-	const [{isMenuShowing, toggleButtonRef}, { toggleIsMenuShowing, handleEscapeKey: handleEscapeKey }] = useMenu();
+const PortalSwitcher = ({ ...props }) => {
+	const [isChangePortalShowing, setIsChangePortalShowing] = useState(false);
+	const toggleButton = useRef(null);
+
+	const toggleHandler = (event) => {
+		if (event.code === 'Enter' || event.code === 'Space') {
+			event.preventDefault();
+			setIsChangePortalShowing(!isChangePortalShowing);
+		}
+	};
+	const escapeKeyHandler = (event) => {
+		if (event.code === 'Escape' && isChangePortalShowing) {
+			setIsChangePortalShowing(false);
+
+			if (toggleButton && toggleButton.current) {
+				toggleButton.current.focus();
+			}
+		}
+	};
 	return (
-		<div className={'portal-switcher'} {...props}>
-			<div onMouseEnter={toggleIsMenuShowing} onMouseLeave={toggleIsMenuShowing}>
+		<div className={'portal-switcher'} {...props} data-testid="portal-switcher">
+			<div
+				onKeyDown={(event) => escapeKeyHandler(event)}
+				onMouseEnter={() => setIsChangePortalShowing(true)}
+				onMouseLeave={() => setIsChangePortalShowing(false)}>
 				<div className="logo-row">
 					<img
 						className="logo"
 						src="https://res.cloudinary.com/dsrqk3ngz/image/upload/v1702580644/insurance-agency-meta_ujrtfn.png"
 						width={75}
-						alt="Company Logo"
+						alt="Insurance Company"
 					/>
 					<div className="text-col">
 						<a href="/account/agents">Insurance Agent Portal</a>
 
-						<button
-							ref={toggleButtonRef}
-							className="change-portal-text focus-visible:ring-2"
-							aria-expanded={isMenuShowing}
-							onClick={toggleIsMenuShowing}
-							onKeyDown={handleEscapeKey}
-							data-testid="change-portal-trigger">
-							Change Portal
-							<IconDownArrow />
-						</button>
+						<div className="change-portal-text" data-testid="change-portal-trigger">
+							<button
+								aria-label="Toggle portal menu"
+								aria-expanded={isChangePortalShowing ? 'true' : 'false'}
+								className="ml-4 focus-visible:ring-2"
+								onKeyDown={(event) => toggleHandler(event)}
+								ref={toggleButton}>
+								Change Portal
+								<FaChevronDown className="icon" />
+							</button>
+						</div>
 					</div>
 				</div>
-				{isMenuShowing && (
-					<div className="inner-portals" onKeyDown={handleEscapeKey}>
+				{isChangePortalShowing && (
+					<div className="inner-portals">
 						<a href="/account/brokers/" className="broker-link" data-testid="broker-portal-link">
 							Broker Portal
 						</a>
@@ -41,20 +62,3 @@ const PortalSwitcher = ({ children, ...props }) => {
 };
 
 export default PortalSwitcher;
-
-function useMenu(toggleButtonRef) {
-	const defaultRef = useRef(null);
-	const ref = toggleButtonRef ?? defaultRef;
-	const [isMenuShowing, setIsMenuShowing] = useState(false);
-	const toggleMenuDisplay = useCallback(() => setIsMenuShowing((prev) => !prev), []);
-	const escapeHandler = useCallback((event) => {
-		if (event.code === "Escape") {
-			setIsMenuShowing(false);
-			if (ref.current) {
-				ref.current.focus();
-			}
-		}
-	}, [ref])
-
-	return [{isMenuShowing, toggleButtonRef: ref}, { toggleIsMenuShowing: toggleMenuDisplay, handleEscapeKey: escapeHandler }];
-}
--- components/PortalSwitcher.test.jsx
+++ components/PortalSwitcher.test.jsx
@@ -6,9 +6,43 @@ import PortalSwitcher from './PortalSwitcher';
 const RenderComponent = ({ ...props }) => render(<PortalSwitcher {...props} />);
 
 describe('Portal Switcher', () => {
-	it('renders', () => {
+	beforeEach(() => {
 		RenderComponent({});
+	})
+	it('renders', () => {
 		const container = screen.getByTestId('portal-switcher');
 		expect(container).toBeInTheDocument();
 	});
+
+	it('should navigate to the Agent landing page', async () => { 
+		const agentPortalLink = screen.getByText(/insurance agent portal/i);
+		expect(agentPortalLink).toHaveProperty("href", expect.stringMatching("/account/agents"));
+	 })
+
+	 it('should show the menu', async () => { 
+		const agentPortalLink = screen.getByText(/insurance agent portal/i);
+		await userEvent.hover(agentPortalLink);
+		const brokerPortalLink = screen.getByText(/broker portal/i);
+		expect(brokerPortalLink).toBeInTheDocument();
+	  })
+
+	  it('should show the menu on tab', async () => { 
+		  const agentPortalLink = screen.getByText(/insurance agent portal/i);
+		await userEvent.tab();
+		expect(agentPortalLink).toHaveFocus();
+		await userEvent.tab();
+		await userEvent.keyboard(" ");
+		const brokerPortalLink = screen.getByText(/broker portal/i);
+		expect(brokerPortalLink).toBeInTheDocument();
+	   })
+	  
+	   it('should hide the menu on escape', async () => { 
+		await userEvent.tab();
+		await userEvent.tab();
+		await userEvent.keyboard(" ");
+		const brokerPortalLink = screen.getByText(/broker portal/i);
+		expect(brokerPortalLink).toBeInTheDocument();
+		await userEvent.keyboard("{Escape}");
+		expect(brokerPortalLink).not.toBeInTheDocument();
+	   })
 });
